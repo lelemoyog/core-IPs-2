9ba65f3f770302db25c170999aba0332bf3f254f
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-07-24 11:40:21 +0300

    added router routes and routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { SearchRepositoryComponent } from './search-repository/search-repository.component' 
+import { componentFactoryName } from '@angular/compiler';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'searchuser', component: SearchUserComponent },
+  { path: 'searchrepository', component: SearchRepositoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
