96f0efbb7f3a74a31c11971747360eb0c9310c5e
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-07-24 11:12:58 +0300

    added a search user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
